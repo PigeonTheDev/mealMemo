0490cf3c3736202e432f339801d65b2d477f3b7e
Feyza Ertürkmen <211805029@stu.adu.edu.tr>
2024-01-06 16:50:03 +0300

    api_task

---
+++ src/API/fake_api.ts
@@ -0,0 +1,39 @@
+interface User {
+    username: string;
+    password: string;
+  }
+  
+  const users: User[] = [{username: "feyza", password: "1234"},{username: "göksu", password: "1234"},{username: "ulaş", password: "1234"},{username: "can", password: "1234"},{username: "celal", password: "1234"},{username: "can", password: "1234"},{username: "berkay", password: "1234"}];
+  
+  const sleep = async (milliseconds: number): Promise<boolean> => {
+    return new Promise((resolve) => setTimeout(() => resolve(true), milliseconds));
+  };
+  
+  const registerUser = async (newUser: User): Promise<string> => {
+    await sleep(1000);
+  
+    const existingUser = users.find((user) => user.username === newUser.username);
+  
+    if (existingUser) {
+      return Promise.reject('User already exists');
+    }
+  
+    users.push(newUser);
+    return Promise.resolve('Registration successful');
+  };
+  
+  const loginUser = async (credentials: User): Promise<User> => {
+    await sleep(1000);
+  
+    const user = users.find(
+      (u) => u.username === credentials.username && u.password === credentials.password
+    );
+  
+    if (user) {
+      return Promise.resolve(user);
+    } else {
+      return Promise.reject('Invalid username or password');
+    }
+  };
+  
+ 
